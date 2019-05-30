ad3c1d8071a80df85bc1458c1aef5a3548e16d71
Jonathan Tan <jon.twk@gmail.com>
2019-05-30 17:01:54 -0500

    another rogue print

--- HW5/pipeline/testtrain.py
+++ HW5/pipeline/testtrain.py
@@ -50,7 +50,7 @@ def train_classifier(df, label, method, df_num, param_dict=None):
         classifier object.
     '''
     print(str(datetime.datetime.now()) + ' Training ' + method + \
-        'with params' + str(param_dict) + 'on training set'  + df_num)
+        'with params' + str(param_dict) + 'on training set'  + str(df_num))
 
     # Supported classifiers
     method_dict = {
