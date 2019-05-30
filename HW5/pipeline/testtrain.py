f9111b4b4798e18d2cc2b1b46c158dd1266a41a3
Jonathan Tan <jon.twk@gmail.com>
2019-05-30 13:48:05 -0500

    removed f strings which is tripping up python on the vm

--- HW5/pipeline/testtrain.py
+++ HW5/pipeline/testtrain.py
@@ -49,7 +49,8 @@ def train_classifier(df, label, method, df_num, param_dict=None):
         (2) hyperparameters used, (3) the test/train id, and (4) the trained
         classifier object.
     '''
-    print(f'{datetime.datetime.now()} Training {method} with params {param_dict} on training set {df_num}')
+    print(str(datetime.datetime.now() + ' Training ' + method +
+        'with params' + param_dict + 'on training set'  + df_num)
 
     # Supported classifiers
     method_dict = {
@@ -75,7 +76,7 @@ def train_classifier(df, label, method, df_num, param_dict=None):
 
     # Raise error if provided method is not supported
     if method not in method_dict.keys():
-        print(f'{method} is not a supported classifier. Pleaes use one of:')
+        print(method + 'is not a supported classifier. Pleaes use one of:')
         print(method_dict.keys())
         return None
 
@@ -116,7 +117,8 @@ def validate_classifier(df, label, classifier, top_k):
     '''
 
     # classifier = (method, param_dict, df_num, trained)
-    print(f'{datetime.datetime.now()} Evaluating {classifier[0]} with {classifier[1]} on top {str(top_k * 100) + "%"}')
+    print(str(datetime.datetime.now() + 'Evaluating ' + classifier[0] + ' with ' \
+        + classifier[1] + ' on top ' +  str(top_k * 100) + '%'})
 
     # Initialize dictionary to store results
     results_dict = {
