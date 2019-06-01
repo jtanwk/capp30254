f2c96238ac9ac5b0e82a8db32fd87cd38c02c30b
Jonathan Tan <jon.twk@gmail.com>
2019-06-01 14:33:29 -0500

    removed unnecessary files

--- HW5/run_pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ./test_env/bin/activate
-
-mkdir output
-python3 hw5_pipeline.py
-
-deactivate
--- HW5/setup_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 -mvenv test_env
-source ./test_env/bin/activate
-
-pip3 install --user --upgrade pip
-pip3 install --user -r requirements.txt
-
-deactivate
