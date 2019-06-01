82be812f0e61baf2b2ac6e0e5a6b1672ec185694
Jonathan Tan <jon.twk@gmail.com>
2019-06-01 14:46:07 -0500

    further sh tweaks

--- HW5/setup_env_rcc.sh
+++ HW5/setup_env_rcc.sh
@@ -5,7 +5,7 @@ python3 -mvenv test_env
 source ./test_env/bin/activate
 
 module load python/3.6.1+intel-16.0
-pip3 install --user --upgrade pip
-pip3 install --user -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 
 deactivate
